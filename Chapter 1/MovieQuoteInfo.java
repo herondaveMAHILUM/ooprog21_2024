9e6b94de0833558187ca3f618a83da6777d0f84a
herondaveMAHILUM <edu.herond04@gmail.com>
2024-08-14 22:28:11 +0800

    Add files via upload

---
+++ Chapter 1/MovieQuoteInfo.java
@@ -0,0 +1,6 @@
+public class MovieQuoteInfo {
+    
+    public static void main(String[] args) {
+        System.out.println("No amount of money ever bought a second of time.\n- Tony Stark \nFrom the movie Avengers: Engame(2019)");
+    }
+}
