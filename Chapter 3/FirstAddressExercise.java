3969553220d07288e33ed8a6db007401932cdac3
herondaveMAHILUM <edu.herond04@gmail.com>
2024-09-18 07:20:30 +0800

    Create FirstAddressExercise.java

---
+++ Chapter 3/FirstAddressExercise.java
@@ -0,0 +1,10 @@
+public class FirstAddressExercise {
+    public static void main(String[] args) {
+        System.out.println("First Java application");
+        displayAddress();
+    }
+    
+    public static void displayAddress() {
+        System.out.println("XYZ Company \n8900 U.S. Hwy 14 \nCrystal Lake, IL 60014 \nCompany: Apple Inc.");
+    }
+}
