e2ee887eda3fc916478ec71581eb67f5a57e19a0
herondaveMAHILUM <edu.herond04@gmail.com>
2024-10-06 22:32:43 +0800

    Update MathTest.java

--- Chapter 4/MathTest.java
+++ Chapter 4/MathTest.java
@@ -1,12 +1,13 @@
 public class MathTest {
    public static void main(String[] args) {
-      System.out.println("SQRT: " + Math.sqrt(64));
-      System.out.println("SIN: " + Math.sin(64));
-      System.out.println("COS: " + Math.cos(64));
-      System.out.println("FLOOR: " + Math.floor(64));
-      System.out.println("CIEL: " + Math.ceil(64));
+      System.out.println("SQRT: " + Math.sqrt(67));
+      System.out.println("SIN: " + Math.sin(270));
+      System.out.println("COS: " + Math.cos(270));
+      System.out.println("FLOOR: " + Math.floor(150));
+      System.out.println("CIEL: " + Math.ceil(150));
       System.out.println("ROUND: " + Math.round(64));
-      System.out.println("MAX: " + Math.max(1, 100));
+      System.out.println("MAX: " + Math.max(68, 71));
+      System.out.println("MIN: " + Math.min(68, 71));
       System.out.println("RANDOM: " + Math.random());
       
    }
